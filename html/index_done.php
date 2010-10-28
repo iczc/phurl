fa89b4186662e4a0df7d12d6db53d13e535c3ffe
Henco <Henco@localhost>
2010-10-28 08:13:53 +0100

    Made post-shortening text (html/index_done.php) more simple and easier to read.

--- html/index_done.php
+++ html/index_done.php
@@ -2,8 +2,6 @@
 ini_set('display_errors', 0);
 ?>
 <h2>Short URL has been created!</h2>
-<p>The following URL:</p>
-<p><strong><?php echo htmlentities($url) ?></strong></p>
-<p>has been length of <strong><?php echo strlen($url) ?></strong> characters and resulted in the following shortened URL which has a length of <strong><?php echo strlen($short_url) ?></strong> characters:</p>
-<p><strong><?php echo htmlentities($short_url) ?></strong><br />
-[ <a href="<?php echo htmlentities($short_url) ?>" target="_blank">Open in new window</a> ]</p>
+<p>Long URL: <strong><?php echo htmlentities($url) ?></strong>(<?php echo strlen($url) ?> characters)</p>
+<p>Short URL: <strong><a href="<?php echo htmlentities($short_url) ?>" target="_blank"><?php echo htmlentities($short_url) ?></a></strong>(<?php echo strlen($short_url) ?> characters)<br />
+
