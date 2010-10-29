8008f79749920a7ccd2e3a91fa148971cb363505
jordan <jordan@phurlproject.org>
2010-10-29 22:45:09 +0100

    Changed captilisation on config file.

--- includes/config.php
+++ includes/config.php
@@ -1,23 +1,24 @@
 <?php
 //Phurl V3 Config File
 //Not directly compatable with v2 Config Files
+$config = array();
 
 //MySQL Config
-$CONFIG['mysql_server'] = 'localhost'; //Mysql Server Hostname/IP
-$CONFIG['mysql_user']   = 'root'; //Mysql Username
-$CONFIG['mysql_pass']   = ''; //MySQL Password
-$CONFIG['mysql_db']     = 'phurl'; //MySQL Database
-$CONFIG['mysql_prefix'] = 'ph_'; //MySQL Table prefix
+$config['mysql_server'] = 'localhost'; //Mysql Server Hostname/IP
+$config['mysql_user']   = 'root'; //Mysql Username
+$config['mysql_pass']   = ''; //MySQL Password
+$config['mysql_db']     = 'phurl'; //MySQL Database
+$config['mysql_prefix'] = 'ph_'; //MySQL Table prefix
 
 //Site Config
-$CONFIG['site_name']    = 'Phurl Version 3'; //Site name
-$CONFIG['site_slogan']	= 'Make your long urls into shorter ones!'; //Site Slogan
-$CONFIG['site_url']		= 'http://locahost'; //URL without trailing slash
-$CONFIG['theme']		= 'default';
+$config['site_name']    = 'Phurl Version 3'; //Site name
+$config['site_slogan']	= 'Make your long urls into shorter ones!'; //Site Slogan
+$config['site_url']		= 'http://locahost'; //URL without trailing slash
+$config['theme']		= 'default';
 
 //Extra
-$CONFIG['debug']		= 'on';
-$CONFIG['version']		= '3.0.1';
+$config['debug']		= true;
+$config['version']		= '3.0.1';
 
 
 ?>
