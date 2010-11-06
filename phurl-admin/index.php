24095ca81fe00d24c19b97ccafcd2fa7825896d6
henry <henry@phurlproject.org>
2010-11-06 15:55:33 +0000

    Started work on admin panel.

---
+++ phurl-admin/index.php
@@ -0,0 +1,31 @@
+<?php
+require_once("../includes/config.php");
+require_once("../includes/functions.php");
+require_once("includes/header.php");
+?>
+<table style="width: 100%">
+	<tr>
+		<td class="txt2" style="width: 250px" valign="top">
+<?php
+require_once("includes/sidebar.php");
+?>
+</td>
+		<td class="txt2">
+<?php
+if($page=="") { 
+$page= $_GET['go']; 
+} 
+$complete="includes/pgcontent/".$page.".php";
+if(file_exists($complete)) { 
+include($complete); 
+}
+elseif($page=="") include ("includes/pgcontent/dashboard.php");
+else { include ("includes/pgcontent/dashboard.php"); }
+?>			
+
+		</td>
+	</tr>
+</table>
+<?php
+require_once("includes/footer.php");
+?>
