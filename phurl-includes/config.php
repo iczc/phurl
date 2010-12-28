7ae70ebe0b82cfbe3be8143535c7b94b424eeb2c
henry <henry@phurlproject.org>
2010-12-28 16:24:56 +0000

    Updated config for new language options

--- phurl-includes/config.php
+++ phurl-includes/config.php
@@ -15,6 +15,7 @@ $config['site_name']    = 'Phurl Version 3'; //Site name
 $config['site_slogan']	= 'Make your long urls into shorter ones!'; //Site Slogan
 $config['site_url']		= 'http://locahost'; //URL without trailing slash
 $config['theme']		= 'default';
+$config['language']		= 'en_GB';
 
 //Extra
 $config['debug']		= true;
