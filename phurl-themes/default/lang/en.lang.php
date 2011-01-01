bdbfd9ea3679747b6c137faab7b9078a07736d19
jordan <jordan@phurlproject.org>
2011-01-01 16:37:50 +0000

    Added Language files in default theme

---
+++ phurl-themes/default/lang/en.lang.php
@@ -0,0 +1,22 @@
+<?php
+//english language file
+$lang = array():
+
+$lang[lang] = "English";
+$lang[1] = "URLs made shorter, URLs made simpler.";
+$lang[2] = "Home";
+$lang[3] = "Tools";
+$lang[4] = "API";
+$lang[5] = "Paste a link to shorten";
+$lang[6] = "Customize";
+$lang[7] = "Add a custom alias";
+$lang[8] = "May contain alphanumeric characters, hyphens and underscores only."
+$lang[9] = "Powered by Phurl";
+$lang[10] = "Please enter a valid URL to shorten";
+$lang[11] = "Short URL has been created";
+$lang[12] = "Long URL";
+$lang[13] = "Short URL";
+$lang[14] = "Characters";
+?>
+
+ 
