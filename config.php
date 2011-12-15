57a675afcea912d51d3fa3075c83952920ffe46f
henry <henry@phurlproject.org>
2011-12-15 01:39:34 +0000

    Upload of initial files, most of which are from V2 and have not yet even been edited.

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'user');
+define('DB_PASSWORD', 'password');
+define('DB_NAME',  'user_db');
+define('DB_VERSION', '4');
+define('DB_PREFIX', 'phurl_');
+define('SITE_URL', 'http://example.org');
+define('SITE_TITLE', 'example.org');
+define('SITE_SLOGAN', 'URLs made shorter, URLs made simpler.');
+define('ADMIN_USERNAME', 'root');
+define('ADMIN_PASSWORD', 'password');
+define('URL_PROTOCOLS', 'http|https|ftp|ftps|mailto|news|mms|rtmp|rtmpt|e2dk');
+define('PHURL_VERSION', '3.0');
+error_reporting(E_ALL);
+$_ERROR = array();
---
+++ html/index_form.php
@@ -0,0 +1,35 @@
+<?php
+if( !defined('PHURL' ) ) {
+    header('HTTP/1.0 404 Not Found');
+    exit();
+}
+ini_set('display_errors', 0);
+?>
+<?php print_errors() ?>
+<div id="create_form">
+<form method="get" action="index.php">
+<div class="input">
+<span class="field">Paste a link to shorten:</span>
+<input id="url" type="text" name="url" value="<?php echo htmlentities(@$_GET['url']) ?>" class="text_box" />
+</div>
+<div class="input">
+		<span class="field" onclick="$('#options').slideToggle('slow', function() {});" id="expand">[+] Customize</span>
+	</div>
+	
+	<div id="options">
+		<div class="input">
+			<span class="field">Add a custom alias (e.g. http://example.org/bicycles) - May contain alphanumeric characters, hyphens and underscores only.</span>
+			<span class="pre">http://example.org/</span><input type="text" name="alias" class="text_box_small" /><br/></p>
+	</div>
+	<div class="clear"></div>
+	</div>
+<br/><input type="submit" value="Shorten" class="submit_button" />
+</form>
+</div>
+<script type="text/javascript">
+//<![CDATA[
+if (document.getElementById) {
+    document.getElementById('url').focus();
+}
+//]]>
+</script>
