88875abf1ecf1f970a4478c1eb2ccb335c4b3ced
henry <henry@phurlproject.org>
2011-12-18 15:17:35 +0000

    Edited go.php to redirect appropriately depending on either stats or unshortening a URL. Fixed a minor interface problem on URL shortening form.

--- go.php
+++ go.php
@@ -1,5 +1,4 @@
 <?php
-header("HTTP/1.1 200 OK");
 require_once("config.php");
 require_once("functions.php");
 
@@ -9,16 +8,21 @@ $getalias = trim(mysql_real_escape_string($_SERVER['REQUEST_URI']));
 $alias = substr($getalias, 1, strlen($getalias));
 
 if (preg_match("/^[a-zA-Z0-9_-]+\-$/", $alias)) {
-  include 'stats/index.php?alias=$alias';
-die();
-} elseif (!preg_match("/^[a-zA-Z0-9]+$/", $alias)) {
+  define('PHURL', true);
+  include "html/header.php";
+  include "stats/index.php";
+  include "html/footer.php";
+  die();
+} elseif (!preg_match("/^[a-zA-Z0-9_-]+$/", $alias)) {
   header("Location: ".SITE_URL, true, 301);
   exit();
-}
+} else {
 
 if (($url = get_url($alias))) {
     header("Location: $url", true, 301);
     exit();
 }
+}
 
 header("Location: ".SITE_URL, true, 301);
+
