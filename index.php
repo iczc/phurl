6fdf2521fda7f3d9396ad86f573039caaad6b610
henry <henry@phurlproject.org>
2012-05-28 00:38:37 +0100

    Defined 'PHURL' on index.php so it *actually works*. Updated theme files with new config format.

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
 // (C) Phurl Project 2012
 
+// Define PHURL to allow includes
+define('PHURL', true);
+
+// No debugging here, oh dear!
+ini_set('display_errors', 0);
+
+//Ensure they've deleted the install directory.
+$prefix[0] = '';
+$filename = 'install';
+if (is_dir($filename)) {
+    die ("To get Phurl up and running, you first need to go through the <a href=\"install\">installation wizard</a> which will help you set up your new URL shortener in a matter of moments.<br/><br/>If you've already installed Phurl, then you MUST delete the install directory before it will function.");
+}
+
 // include the config file
 include "includes/config.php";
 
